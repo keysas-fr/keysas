574f43587b4549ff8decce3be0c6858249e08ad3
Harvester57 <florian.stosse@gmail.com>
2025-11-30 16:05:24 +0100

    Fix Rustfmt warning

--- keysas-core/src/keysas-transit/main.rs
+++ keysas-core/src/keysas-transit/main.rs
@@ -466,10 +466,7 @@ fn send_files(files: &Vec<FileData>, stream: &UnixStream) {
         let mut ancillary = SocketAncillary::new(&mut ancillary_buffer);
         ancillary.add_fds(&[file.fd][..]);
         match stream.send_vectored_with_ancillary(&bufs[..], &mut ancillary) {
-            Ok(_) => info!(
-                "File {} sent to Keysas-out.",
-                file.md.filename
-            ),
+            Ok(_) => info!("File {} sent to Keysas-out.", file.md.filename),
             Err(e) => error!("Failed to send file {e}."),
         }
         // Close the file descriptor
